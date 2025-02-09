20e9b200f19137a39e48676558b0bb766c5c2f74
“Priyanshu <priyanshukumar8232@gmail.com>
2025-02-09 23:17:51 +0530

    new files

--- SRC/seedFirestore.js
+++ SRC/seedFirestore.js
@@ -8,7 +8,7 @@ const rentalProducts = [
     pricePerDay: 50,
     imageUrl: "https://in.canon/media/image/2020/07/04/862a1f43feed4fab85897aee45b6324c_R5_Front_BODY.png",
     description: "High-end mirrorless camera with 8K video recording.",
-    location: "India",
+    location: "Kolkata, India",
     available: true
   },
   {
@@ -17,7 +17,7 @@ const rentalProducts = [
     pricePerDay: 30,
     imageUrl: "https://shop.yamaha-motor-india.com/cdn/shop/products/Black_720x720.webp?v=1680862102",
     description: "A sporty bike perfect for city rides.",
-    location: "India",
+    location: "Jabalpur, India",
     available: true
   },
   {
@@ -35,7 +35,7 @@ const rentalProducts = [
     pricePerDay: 40,
     imageUrl: "https://5.imimg.com/data5/SELLER/Default/2021/9/ZA/TY/UV/135768661/mavic-air2-dji-drone-camera.jpg",
     description: "Compact drone with 4K video recording.",
-    location: "India",
+    location: "Lucknow, India",
     available: true
   },
   {
@@ -44,7 +44,7 @@ const rentalProducts = [
     pricePerDay: 60,
     imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6PRncf0cSUfaMPOb-CCDl6FRcyubHfC56BA&s",
     description: "Powerful laptop for professional use.",
-    location: "India",
+    location: "Delhi, India",
     available: true
   }
 ];
